301ac8273db6f0cb94e269f16bfc936cea6ed2e5
Antoan Matein <antoandmatein@gmail.com>
2025-08-02 16:24:01 +0300

    Test of Rust module

--- trader/main.py
+++ trader/main.py
@@ -2,13 +2,29 @@
 
 import cpp_engine
 import rust_data_pipeline
+import json
+
+API_PATH = "trader/config.json"
+SYMBOL='AAPL'
+OUTPUT_PATH = f"data/{SYMBOL}_data.bin"
+with open(API_PATH, 'r') as f:
+    API_KEY = json.load(f)['api_key']
 
 def run():
     print('--- Starting backtest ---')
 
     # Rust part
     print('1. Calling Rust data pipeline...')
-    data_path = rust_data_pipeline.fetch_and_clean("AAPL", "data/processed/AAPL.bin")
+    
+    try:
+        data_path = rust_data_pipeline.fetch_and_clean(
+        symbol=SYMBOL,
+        api_key=API_KEY,
+        output_path=OUTPUT_PATH
+        )
+        print(f"Successfully fetched and saved data to {OUTPUT_PATH}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
     # C++ part
     print('2. [Dummy] C++ engine ran successfully.')
